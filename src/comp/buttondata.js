11519ac73f26a186b2f497ed9ebe7db6915dcffd
Kai Gay <gayk@oregonstate.edu>
2020-08-27 21:45:24 -0700

    fixed?

--- src/comp/buttondata.js
+++ src/comp/buttondata.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRandom, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { Button, ButtonGroup } from 'reactstrap';
+import {postGameByIdVoteShuffle} from '../Api.js'
 
 const shuffle = <FontAwesomeIcon icon={faRandom} />
 const launch = <FontAwesomeIcon icon={faPlay} />
 
+
 const ButtonData = () => {
+    let voteForShuffle = (e) => {
+        let id = localStorage.getItem('steamid'); // fix me not good :(
+        postGameByIdVoteShuffle(0, {id: id}).then(
+            console.log("VOTED SHUFFLE!!"));
+    }
     return(
         <ButtonGroup vertical size="md">
-            <Button className="btn btn-shuffle">
+            <Button className="btn btn-shuffle" onClick={voteForShuffle}>
                 <span>
                     <i>{shuffle} Shuffle Teams</i>
                 </span>
