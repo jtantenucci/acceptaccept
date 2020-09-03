010b2a0d6cdde02fb91da5db7e1e8159319aea8b
Kai Gay <gayk@oregonstate.edu>
2020-09-03 05:43:16 -0700

    moved state up

--- src/comp/matchlobby.js
+++ src/comp/matchlobby.js
@@ -3,8 +3,92 @@ import TableData from './tabledata';
 import MapVoteModal from './mapvote';
 import ButtonData from './buttondata';
 import AcceptJumbotron from './jumbotron';
+import {getGameById, postGameByIdTeamA, postGameByIdTeamB, postGameByIdReady, deleteGameById} from '../Api'
+import {w3cwebsocket as W3CWebSocket} from "websocket"
 
 class MatchLobby extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: false,
+            queue: null
+        };
+
+      this.gameid = 0;
+      this.update = this.update.bind(this);
+      getGameById(this.gameid).then(res => {
+        console.log("initial load");
+        console.log(res);
+        this.setState({queue: res.data})
+       this.clientid = localStorage.getItem('steamid');
+     });
+    }
+    websocketconn = () =>
+    {
+      const client = new W3CWebSocket("ws://queue.boner1.com/ws/game/0/sub")
+      client.onclose = x => {
+          console.log('lost connection... retrying');
+          setTimeout(this.websocketconn, 1000);
+      };
+      client.onopen = () => {
+        console.log("subscribed!")
+      }
+      client.onmessage = (message) => {
+        let data = JSON.parse(message.data);
+        this.setState({queue: data});
+    }
+    }
+   componentWillMount() {
+     this.websocketconn();
+  }
+    update = () => {
+      getGameById(this.gameid).then(game => {
+        this.setState({queue: game.data});
+      });
+    }
+
+    handleClick = (i, team) => (e) => {
+
+      if(this.clientid)
+        {
+          if(team === "teamA") {
+            postGameByIdTeamA(this.gameid, [i, this.clientid])
+              .then(game => {
+                this.setState({
+                  queue: game.data
+                });
+              })
+              .catch(err => console.log(err))
+            }
+          else {
+            postGameByIdTeamB(this.gameid, [i, this.clientid])
+              .then(game => {
+                this.setState({
+                  queue: game.data
+                });
+              })
+              .catch(err => console.log(err))
+            }
+        }
+    }
+  readyUp = (e) => {
+    console.log(this.clientid);
+    console.log(typeof this.clientid)
+    e.stopPropagation();
+    postGameByIdReady(0, {id:this.clientid}).then(game => {
+      this.setState({
+        queue: game.data
+      });
+    });
+  }
+  leaveMatch = (e) => {
+    e.stopPropagation();
+    deleteGameById(0, {id: this.clientid}).then(game => {
+      this.setState({
+        queue: game.data
+      });
+    })
+  }
     render () {
         return (
             <div>
@@ -21,7 +105,12 @@ class MatchLobby extends Component {
                             <h2 className="head">team 2</h2>
                         </div>
                     </div>
-                    <TableData />
+                    <TableData
+                      queue={this.state.queue}
+                      readyUp={this.readyUp}
+                      leave={this.leaveMatch}
+                      handleClick={this.handleClick}
+                    />
                     <br></br>
                     <div className="row row-buttons">
                         <div className="col vote">
--- src/comp/tabledata.js
+++ src/comp/tabledata.js
@@ -1,95 +1,14 @@
 import React from 'react';
 import { Card } from 'reactstrap';
 import { PlayerSlot } from './playerlobby/playerslot'
-import {getGameById, postGameByIdTeamA, postGameByIdTeamB, postGameByIdReady, deleteGameById} from '../Api'
-import {w3cwebsocket as W3CWebSocket} from "websocket"
-
 import { withRouter } from 'react-router-dom';
 /* global BigInt */
 export class TableData extends React.Component {
 
     constructor(props) {
-        super(props);
-        this.state = {
-            isActive: false,
-            queue: null
-        };
-
-      this.gameid = 0;
-      this.update = this.update.bind(this);
-      getGameById(this.gameid).then(res => {
-        console.log("initial load");
-        console.log(res);
-        this.setState({queue: res.data})
-       this.clientid = localStorage.getItem('steamid');
-     });
+      super(props);
+      this.clientid = localStorage.getItem('steamid');
     }
-    websocketconn = () =>
-    {
-      const client = new W3CWebSocket("ws://queue.boner1.com/ws/game/0/sub")
-      client.onclose = x => {
-          console.log('lost connection... retrying');
-          setTimeout(this.websocketconn, 1000);
-      };
-      client.onopen = () => {
-        console.log("subscribed!")
-      }
-      client.onmessage = (message) => {
-        let data = JSON.parse(message.data);
-        this.setState({queue: data});
-    }
-    }
-   componentWillMount() {
-     this.websocketconn();
-  }
-    update = () => {
-      getGameById(this.gameid).then(game => {
-        this.setState({queue: game.data});
-      });
-    }
-    
-    handleClick = (i, team) => (e) => {
-
-      if(this.clientid)
-        {
-          if(team === "teamA") {
-            postGameByIdTeamA(this.gameid, [i, this.clientid])
-              .then(game => {
-                this.setState({
-                  queue: game.data
-                });
-              })
-              .catch(err => console.log(err))
-            }
-          else {
-            postGameByIdTeamB(this.gameid, [i, this.clientid])
-              .then(game => {
-                this.setState({
-                  queue: game.data
-                });
-              })
-              .catch(err => console.log(err))
-            }
-        }
-    }
-  readyUp = (e) => {
-    console.log(this.clientid);
-    console.log(typeof this.clientid)
-    e.stopPropagation();
-    postGameByIdReady(0, {id:this.clientid}).then(game => {
-      this.setState({
-        queue: game.data
-      });
-    });
-  }
-  leaveMatch = (e) => {
-    e.stopPropagation();
-    deleteGameById(0, {id: this.clientid}).then(game => {
-      this.setState({
-        queue: game.data
-      });
-    })
-  }
    render () {
     var rows = []
     for (var i = 0; i < 5; i++) {
@@ -97,22 +16,22 @@ export class TableData extends React.Component {
           <React.Fragment>
               <div className="row lobby-players">
               <PlayerSlot gameid="0"
-                          user={this.state.queue?.teamA?.[i]}
-                          setReady={this.readyUp}
-                          leave={this.leaveMatch}
-                          handleClick={this.handleClick(i, "teamA")}/>
+                          user={this.props.queue?.teamA?.[i]}
+                          setReady={this.props.readyUp}
+                          leave={this.props.leaveMatch}
+                          handleClick={this.props.handleClick(i, "teamA")}/>
               <PlayerSlot gameid="0"
-                          user={this.state.queue?.teamB?.[i]}
-                          leave={this.leaveMatch}
-                          setReady={this.readyUp}
-                          handleClick={this.handleClick(i, "teamB")}/>              </div>
+                          user={this.props.queue?.teamB?.[i]}
+                          leave={this.props.leaveMatch}
+                          setReady={this.props.readyUp}
+                          handleClick={this.props.handleClick(i, "teamB")}/>              </div>
           </React.Fragment>
         );
       }
       
     return (
       <div className="container">
-        <Card className="lobby-slot-card" onClick={this.update}>
+        <Card className="lobby-slot-card" onClick={this.props.update}>
             {rows}
         </Card>
       </div>
